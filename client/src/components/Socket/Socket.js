5d2b485520bc2bcba90b549d63c6a9f30f275ffe
abeeral-rafati <abeerw.rafati@yahoo.com>
2021-07-18 00:39:54 +0300

    heroku link

--- client/src/components/Socket/Socket.js
+++ client/src/components/Socket/Socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
 const SERVER_URL = process.env.SERVER_URL || "localhost:5000/";
+// const SERVER_URL = process.env.SERVER_URL || "https://whiteboard-pseudo-project-401.herokuapp.com/";
 
 export const socket = io(SERVER_URL, {
   transports: ["websocket"],
