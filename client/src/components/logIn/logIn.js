9b3cee95055436aa63cd38a7aa75e73a823d4293
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-07-17 17:57:33 +0300

    loginCss

--- client/src/components/logIn/logIn.js
+++ client/src/components/logIn/logIn.js
@@ -1,41 +1,58 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
-
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Modal from "react-bootstrap/Modal";
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+import "../logIn/login.css";
 export class LogIn extends Component {
     render() {
         return (
-            <>
-                <Modal.Dialog>
-                    <Modal.Header  >
-                        <Modal.Title>Register in Student Class</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form>
-                            <Form.Label > ADD YOUR NAME </Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="name"
-                                placeholder="your name ...."
-                                required
-                            />
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary">CLOSE</Button>
-                        {/* <Button variant="primary" onClick={() => { }}>LOGIN</Button> */}
-                        <Button variant="warning"> <Link className="button" activeClassName="is-active" to="/student">
-                            Login
-                        </Link>  </Button>
-                    </Modal.Footer>
-                </Modal.Dialog >
-
-
-
-            </>
+            //   <>
+            //     <Modal.Dialog>
+            //       <Modal.Header>
+            //         <Modal.Title>Register in Student Class</Modal.Title>
+            //       </Modal.Header>
+            //       <Modal.Body>
+            //         <Form>
+            //           <Form.Label> ADD YOUR NAME </Form.Label>
+            //           <Form.Control
+            //             type="text"
+            //             name="name"
+            //             placeholder="your name ...."
+            //             required
+            //           />
+            //         </Form>
+            //       </Modal.Body>
+            //       <Modal.Footer>
+            //         <Button variant="secondary">CLOSE</Button>
+            //         {/* <Button variant="primary" onClick={() => { }}>LOGIN</Button> */}
+            //         <Button variant="warning">
+            //           {" "}
+            //           <Link
+            //             className="button"
+            //             activeClassName="is-active"
+            //             to="/student"
+            //           >
+            //             Login
+            //           </Link>{" "}
+            //         </Button>
+            //       </Modal.Footer>
+            //     </Modal.Dialog>
+            //   </>
+            <div className="container" onclick="onclick">
+                <div className="top"></div>
+                <div className="bottom"></div>
+                <div className="center">
+                    <h2>Please Sign In</h2>
+                    <input type="text" placeholder="your name ..." />
+                    {/* <input type="password" placeholder="password" /> */}
+                    <Link className="button" activeClassName="is-active" to="/student">
+                        Login
+                    </Link>
+                    <h2>&nbsp;</h2>
+                </div>
+            </div>
         );
     }
 }
